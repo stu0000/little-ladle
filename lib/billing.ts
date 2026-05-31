3a76b753cfe02e2d675cdee4b8405a702105f597
Stuart <stuartmcqueenthomson@gmail.com>
2026-06-01 08:29:57 +1000

    Update Stripe API version

--- lib/billing.ts
+++ lib/billing.ts
@@ -9,7 +9,7 @@ import Stripe from 'stripe';
  */
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16',
+  apiVersion: '2026-05-27',
 });
 
 export interface BillingPlan {
