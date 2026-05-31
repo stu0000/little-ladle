6f1e5a91853d2a73b8cc030cd2db7a9215c02083
Stuart <stuartmcqueenthomson@gmail.com>
2026-06-01 08:37:33 +1000

    Fix Stripe API version format

--- lib/billing.ts
+++ lib/billing.ts
@@ -9,7 +9,7 @@ import Stripe from 'stripe';
  */
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2026-05-27',
+  apiVersion: '2026-05-27.dahlia',
 });
 
 export interface BillingPlan {
